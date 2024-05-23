1179625b20d0e0926e2087ecb0b23f40f3ad8ea5
HariPrasad0023 <c.hariprasad@iiitb.ac.in>
2024-05-23 20:31:00 +0530

    Modified config.js file.

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://anime-service:31000";
+const API_URL = "http://anime-service:9292";
 // const API_URL = "https://e5c5-103-156-19-229.ngrok-free.app";
 
 export default API_URL;
