3d6f96c69a1c474049ecba77763c79c210f3f6bb
HariPrasad0023 <c.hariprasad@iiitb.ac.in>
2024-05-23 15:08:26 +0530

    Modified AnimeController.java and CrosConfig.java file.

--- Anime/src/main/java/com/review/anime/Controller/AnimeController.java
+++ Anime/src/main/java/com/review/anime/Controller/AnimeController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Random;
 
 @RestController
-@CrossOrigin("http://localhost:9292")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/api/anime", produces = "application/json")
 public class AnimeController {
 
